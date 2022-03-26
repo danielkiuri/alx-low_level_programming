9e69956cf216b1e95778bb0ea23713a9af14c7bc
Daniel Kiuri <danielkiuri@outlook.com>
2022-03-26 09:44:28 +0300

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - print alphabets
+*
+* Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+
+}
+

d95da825281778c4dd6ec8b7772d64c3c59c1ebf
Daniel Kiuri <danielkiuri@outlook.com>
2022-03-26 10:29:11 +0300

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
 * print_last_digit - prints the last digit of a number
 * @n: number being tested
-* Return: Always 0
+* Return: Always 0.
 */
 
 int print_last_digit(int n)
@@ -11,7 +11,10 @@ int print_last_digit(int n)
 	n = n % 10;
 
 	if (n < 0)
+	{
 		n = -n;
 		_putchar(n + '0');
-		return (n);
+	}
+	return (n);
 }
+

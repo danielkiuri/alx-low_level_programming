8afa614aaf91210788804748314822c50b9a560d
Daniel Kiuri <danielkiuri@outlook.com>
2022-03-29 07:43:08 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memset - ironnou
+* @s: pointer to char params
+* @b: data to change
+* @n: index
+* Return: *s
+*/
+
+char *_memset(chhar *s, char b, unsigned int n)
+{
+	unsigned int 1;
+
+	for (i = 0, i < n, i++)
+	{
+		s[1] = b;
+	}
+	return (s);
+}

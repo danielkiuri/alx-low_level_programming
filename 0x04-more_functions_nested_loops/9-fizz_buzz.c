940689546a93a323cf9ae90694aaca218db29413
Daniel Kiuri <danielkiuri@outlook.com>
2022-04-23 09:38:21 +0300

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int i;
 
-	printf("%d", 1);
+	printf(" %d", 1);
 	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else
-			printf("%d", 1);
+			printf(" %d", 1);
 	}
 	printf("\n");
 	return (0);
